92da131ce3961aa08ece781db8505dd9d7d62f39
DeineStrainReviews <admin@deinestrainreviews.de>
2025-03-15 10:40:52 +0100

    fix

--- scroll-functions.js
+++ scroll-functions.js
@@ -1,18 +1,20 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var scrollLinks = document.querySelectorAll(".scroll-link");
+if (typeof window !== "undefined" && typeof document !== "undefined") {
+  document.addEventListener("DOMContentLoaded", function () {
+    var scrollLinks = document.querySelectorAll(".scroll-link");
 
-  Array.prototype.forEach.call(scrollLinks, function (link) {
-    link.addEventListener("click", function (event) {
-      event.preventDefault();
-      var targetId = this.getAttribute("href").substring(1);
-      var targetElement = globalThis?.document?.getElementById(targetId);
+    Array.prototype.forEach.call(scrollLinks, function (link) {
+      link.addEventListener("click", function (event) {
+        event.preventDefault();
+        var targetId = this.getAttribute("href").substring(1);
+        var targetElement = globalThis?.document?.getElementById(targetId);
 
-      if (targetElement) {
-        window.scrollTo({
-          top: targetElement.offsetTop,
-          behavior: "smooth",
-        });
-      }
+        if (targetElement) {
+          window.scrollTo({
+            top: targetElement.offsetTop,
+            behavior: "smooth",
+          });
+        }
+      });
     });
   });
-});
+}
