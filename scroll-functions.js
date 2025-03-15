64b784f9ed3aba6b1673d5983ef0272f2f3bf8b1
DeineStrainReviews <admin@deinestrainreviews.de>
2025-03-15 10:36:22 +0100

    fixed errors

--- scroll-functions.js
+++ scroll-functions.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const scrollLinks = document.querySelectorAll(".scroll-link");
+  var scrollLinks = document.querySelectorAll(".scroll-link");
 
-  scrollLinks.forEach((link) => {
+  Array.prototype.forEach.call(scrollLinks, function (link) {
     link.addEventListener("click", function (event) {
       event.preventDefault();
-      const targetId = this.getAttribute("href").substring(1);
-      const targetElement = document.getElementById(targetId);
+      var targetId = this.getAttribute("href").substring(1);
+      var targetElement = globalThis?.document?.getElementById(targetId);
 
       if (targetElement) {
         window.scrollTo({
