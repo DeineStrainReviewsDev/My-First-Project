0e2463806df6ad48171477138f6f2b121138652a
DeineStrainReviews <admin@deinestrainreviews.de>
2025-03-15 11:19:35 +0100

    fix

--- scroll-functions.js
+++ scroll-functions.js
@@ -1,12 +1,19 @@
-if (typeof window !== "undefined" && typeof document !== "undefined") {
-  document.addEventListener("DOMContentLoaded", function () {
-    var scrollLinks = document.querySelectorAll(".scroll-link");
+if (typeof window !== "undefined") {
+  window.addEventListener("DOMContentLoaded", function () {
+    var scrollLinks =
+      typeof document !== "undefined"
+        ? document.querySelectorAll(".scroll-link")
+        : [];
 
     Array.prototype.forEach.call(scrollLinks, function (link) {
       link.addEventListener("click", function (event) {
         event.preventDefault();
+
         var targetId = this.getAttribute("href").substring(1);
-        var targetElement = document ? document.getElementById(targetId) : null;
+        var targetElement =
+          typeof document !== "undefined"
+            ? document.getElementById(targetId)
+            : null;
 
         if (targetElement) {
           window.scrollTo({
