e765db4571583e5125edea34c80db16e156150a8
DeineStrainReviews <admin@deinestrainreviews.de>
2025-03-15 11:16:52 +0100

    fix

--- scroll-functions.js
+++ scroll-functions.js
@@ -6,10 +6,7 @@ if (typeof window !== "undefined" && typeof document !== "undefined") {
       link.addEventListener("click", function (event) {
         event.preventDefault();
         var targetId = this.getAttribute("href").substring(1);
-        var targetElement =
-          typeof document !== "undefined"
-            ? document.getElementById(targetId)
-            : null;
+        var targetElement = document ? document.getElementById(targetId) : null;
 
         if (targetElement) {
           window.scrollTo({
