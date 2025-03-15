9745db10d9a46b48b4d124f3795ce9f747c2d224
DeineStrainReviews <17227224+DeineStrainReviewsDev@users.noreply.github.com>
2025-03-15 21:58:22 +0100

    Dev patch 9 (#20)

---
+++ mode-switcher.js
@@ -0,0 +1,24 @@
+/* eslint-disable n/no-unsupported-features/node-builtins */
+
+if (typeof window !== "undefined") {
+  window.addEventListener("DOMContentLoaded", function () {
+    var toggleButton = document.getElementById("mode-toggle-btn");
+    var body = document.body;
+
+    if (window.localStorage) {
+      if (window.localStorage.getItem("darkMode") === "enabled") {
+        body.classList.add("dark-mode");
+      }
+
+      toggleButton.addEventListener("click", function () {
+        body.classList.toggle("dark-mode");
+
+        if (body.classList.contains("dark-mode")) {
+          window.localStorage.setItem("darkMode", "enabled");
+        } else {
+          window.localStorage.setItem("darkMode", "disabled");
+        }
+      });
+    }
+  });
+}
